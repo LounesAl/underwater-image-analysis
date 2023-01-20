c1652114b024c8fe4ab57aa51ab889a10dc39f3a
LounesAl <lounas.allioui@hotmail.fr>
2023-01-20 21:18:41 +0100

    Update

--- stereo_calibration/3D_points.py
+++ stereo_calibration/3D_points.py
@@ -15,11 +15,10 @@ print(uvs2)
 # Show the two images with their 2D points
 show_scatter_2D(frame1, frame2, uvs1, uvs2)
 
-
 """ The next step is to obtain the projection matrices. This is done simply by multiplying the camera matrix by the rotation and translation matrix. """
 
 # Load the result of the calibration
-mtx1, mtx2, R, T = np.load('stereo_calibration/stereo_calibration.npy')
+mtx1, mtx2, R, T = np.load('stereo_calibration/camera_parameters/stereo_params.npy', allow_pickle=True)
 
 # Calculate the projection martrix
 P1, P2 = get_projection_matrix(mtx1, mtx2, R, T)
