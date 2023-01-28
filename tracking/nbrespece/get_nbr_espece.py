f71b691884b2af7cc6ac718e79fd38b51567ac60
amelmzt <117472245+amelmzt@users.noreply.github.com>
2023-01-29 00:19:05 +0100

    update

---
+++ tracking/nbrespece/get_nbr_espece.py
@@ -0,0 +1,48 @@
+
+from glob import glob
+
+import numpy as np
+import os 
+
+
+## voir les classes predites
+    ## le resultat est de la forme : tensor([0, 1, 1, 2, 3, 3]), cela veut dire :
+    ## une espces PFE, deux actinia fermées, une ouverte et deux gibbula
+
+#class_dict = {
+#            "0" : "PFE",
+#            "1" : "Actinia fermee",
+#            "2" : "Actinia ouverte",
+#            "3" : "Gibbula"
+#        }
+
+def get_nbr_espece (tensor) : 
+
+
+
+    
+    return nbr_espece 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+
+
+
+
+
+
+
