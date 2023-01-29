b8ed51b052d47468aec2e239821e9a9e7c6d6d77
amelmzt <117472245+amelmzt@users.noreply.github.com>
2023-01-29 02:29:08 +0100

    update

--- tracking/nbrespece/get_nbr_espece.py
+++ tracking/nbrespece/get_nbr_espece.py
@@ -20,23 +20,28 @@ def get_nbr_espece (tensor, nbr_classe) :
 
     taille_tensor = len(tensor)
 
-    nbr_classe_ =  np.empty((nbr_classe,1))
+    nbr_classe_ =  np.empty((nbr_classe+1,1))
 
-    for i in range (nbr_classe) : 
+    for i in range (nbr_classe+1) : 
         #exec("nbr_classe_" + str(i) + " = 0")
         nbr_classe_[i] = 0
 
-    for i in range (taille_tensor) : 
-        pass
+    for elem in tensor : 
+        for i in range (1 , nbr_classe+1) : 
+            if (elem == i) : 
+                nbr_classe_[i] = nbr_classe_[i] + 1 
 
 
+    print(nbr_classe_[3])
 
     #return nbr_espece 
 
+#ce qui reste a faire associer les classes aux nbr de fois qu'on l'a detecter 
+#penser a comment faire un plotlib 
 
 if __name__ == "__main__":
 
-    tensor = [0, 1, 1, 2, 3, 3]
+    tensor = [0, 1, 1, 2, 3, 3 , 3]
     nbr_classe = 3
 
     get_nbr_espece (tensor, nbr_classe)
